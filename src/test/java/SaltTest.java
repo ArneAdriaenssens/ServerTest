fcc05282bd9a8428a2f07aaf5750cda9ad507fbb
ArneAdriaenssens <arne.adriaenssens@hotmail.com>
2016-02-25 14:20:14 +0100

    refactor

---
+++ src/test/java/SaltTest.java
@@ -0,0 +1,32 @@
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Arne on 25/02/2016.
+ */
+public class SaltTest {
+
+    private Salt testObject;
+
+    @Before
+    public void setUp(){
+        testObject=new Salt();
+    }
+
+    @Test
+    public void test_generateSalt(){
+        testObject.generateSalt();
+        assertTrue(true);
+    }
+
+    @Test
+    public void test_compareSalt(){
+        testObject.compareSalt(testObject.generateSalt());
+        assertTrue(true);
+    }
+
+}
